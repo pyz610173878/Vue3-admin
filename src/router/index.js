90a9e841e5584d1802cbc40726546eca88be4903
hacker <610173878@qq.com>
2023-05-24 19:17:41 +0800

    second

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import baseRouters from './modules/base';
+import baseRouters from './modules/index_back';
 
 const routes = [...baseRouters];
 
